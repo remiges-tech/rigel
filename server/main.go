b46da3d9f677e894b9ec8f17314ef8b58575db1d
Sachin Divekar <sachin.divekar@remiges.tech>
2024-01-19 22:21:02 +0530

    Removed cordMiddlware() from main

--- server/main.go
+++ server/main.go
@@ -116,16 +116,3 @@ func main() {
 	}
 
 }
-
-func corsMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
-			return
-		}
-		c.Next()
-	}
-}
