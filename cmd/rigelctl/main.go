948771786d74eb76862ce0a9fae8c3f25464cdfe
Sachin Divekar <sachin.divekar@remiges.tech>
2024-01-22 23:56:28 +0530

    Add cli arguments to set app and module for the current command context

--- cmd/rigelctl/main.go
+++ cmd/rigelctl/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"strconv"
+	"strings"
+	"unsafe"
 
 	"github.com/remiges-tech/rigel/cmd/rigelctl/rigelctl"
 
@@ -11,19 +15,37 @@ import (
 )
 
 func main() {
-	// Create a new EtcdStorage instance
-	etcdStorage, err := etcd.NewEtcdStorage([]string{"localhost:2379"})
-	if err != nil {
-		log.Fatalf("Failed to create EtcdStorage: %v", err)
-	}
-	// Create a new Rigel instance
-	rigelClient := rigel.New(etcdStorage, "testapp", "testmodule", 1, "testconfig")
+	var etcdEndpoint, app, module string
 
 	// Create the root command
 	rootCmd := &cobra.Command{
 		Use:   "rigelctl",
 		Short: "CLI for managing Rigel schemas and configs",
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			// Split the etcdEndpoint string into a slice of strings
+			etcdEndpoints := strings.Split(etcdEndpoint, ",")
+
+			// Create a new EtcdStorage instance
+			etcdStorage, err := etcd.NewEtcdStorage(etcdEndpoints)
+			if err != nil {
+				return fmt.Errorf("Failed to create EtcdStorage: %v", err)
+			}
+
+			// Create a new Rigel instance with the provided Storage interface
+			rigelClient := rigel.NewWithStorage(etcdStorage)
+
+			// Set the App and Module fields using the WithApp and WithModule methods
+			rigelClient = rigelClient.WithApp(app).WithModule(module)
+
+			// Store the Rigel client pointer in the command's annotations for later retrieval
+			cmd.Annotations = make(map[string]string)
+			cmd.Annotations["rigelClient"] = fmt.Sprintf("%p", rigelClient)
+			return nil
+		},
 	}
+	rootCmd.PersistentFlags().StringVarP(&etcdEndpoint, "etcd-endpoint", "e", "localhost:2379", "etcd endpoint")
+	rootCmd.PersistentFlags().StringVarP(&app, "app", "a", "testapp", "app name")
+	rootCmd.PersistentFlags().StringVarP(&module, "module", "m", "testmodule", "module name")
 
 	// Create the 'schema' command
 	schemaCmd := &cobra.Command{
@@ -37,10 +59,17 @@ func main() {
 		Short: "Add a new schema from a file",
 		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
+			rigelClientPtr, _ := strconv.ParseUint(cmd.Annotations["rigelClient"], 0, 64)
+			rigelClient := (*rigel.Rigel)(unsafe.Pointer(uintptr(rigelClientPtr)))
+
+			// Check if the rigelClient is nil
+			if rigelClient == nil {
+				return fmt.Errorf("Failed to initialize Rigel client")
+			}
+
 			return rigelctl.AddSchemaCommand(rigelClient, cmd, args)
 		},
 	}
-
 	// Add the 'addSchema' command to the 'schema' command
 	schemaCmd.AddCommand(addSchemaCmd)
 
@@ -51,5 +80,4 @@ func main() {
 	if err := rootCmd.Execute(); err != nil {
 		log.Fatalf("Failed to execute root command: %v", err)
 	}
-
 }
